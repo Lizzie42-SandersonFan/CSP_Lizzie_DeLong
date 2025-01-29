dfb54dca06466c46c7dbace9d8f18734b5d3fe9c
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-01-29 11:49:10 -0700

    Python varible practice

---
+++ Varibles/practice.py
@@ -0,0 +1,15 @@
+name = "Lizzie"
+number = 6
+biggerNumber = 765
+breakfast = "Bread and Butter"
+favColour = "Red"
+schoolName = "UCAS"
+year = 2025
+eyeColour = "Blue"
+age = 15
+favSubject = "Math or CSP"
+
+print("My name is", name, "and I have", eyeColour, "eyes and I like the colour", favColour, "I am also", age)
+print("I had", breakfast, "for breakfast.")
+print("I got to", schoolName, "and my favcorite subjects are", favSubject, "and it is", year)
+print("some number are", number, "and", biggerNumber)
