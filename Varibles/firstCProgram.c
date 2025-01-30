fcb0ec0e9c2fbf77f774aa6e72375396c0b773b9
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-01-30 11:48:39 -0700

    final C work

--- Varibles/firstCProgram.c
+++ Varibles/firstCProgram.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    
-    
+    //printf("Hello World");
+    char greating[] = "Hello! I would like to know you more!";
+    printf(greating);
+
+    char yourName[] = "Lizzie";
+    char favBook[] = "Great! Now tell me you favorite book: \n";
+    scanf(%s %s, yourName, favBook)
+
+    printf("Thank you! Now I know you name %s is and you like the book %s\n", yourName, favBook);
+
     return 0;
 }
