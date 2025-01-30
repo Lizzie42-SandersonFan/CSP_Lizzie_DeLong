c4fbd4859c2ec19708bbc9ed05e40c36b5f9faf3
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-01-30 11:34:48 -0700

    Work on first programs

---
+++ Varibles/firstPythonProgram.py
@@ -0,0 +1,7 @@
+greating = "Hello! I would like to know you more!"
+print(greating)
+
+name = input("Would you tell me your name please: ")
+favBook = input("Great! Now tell me you favorite book: ")
+
+print("Thank you! Now I know you name is", name, "and you like the book", favBook)
