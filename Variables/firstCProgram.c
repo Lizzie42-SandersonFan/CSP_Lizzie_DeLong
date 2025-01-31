5366071e9486ddb9c706192571a73c3577ff76e1
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-01-31 11:48:28 -0700

    Working

--- Variables/firstCProgram.c
+++ Variables/firstCProgram.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    //printf("Hello World");
-    char greating[] = "Hello! I would like to know you more!";
+    char greating[] = "Hello! I would like to know you more! \n";
     printf(greating);
+    printf("Would you please tell me your name:\n");
 
     char yourName[30];
     scanf("%s", yourName);
-    printf("Hello %s", yourName);
+ 
 
     char favBook[60];
-    scanf("%s", favBook);
     printf("Great! Now tell me you favorite book: \n");
+    fgets(favBook, sizeof(favBook), stdin);
+    //scanf("%s", favBook);
+ 
     
 
     printf("Thank you! Now I know you name %s is and you like the book %s\n", yourName, favBook);
