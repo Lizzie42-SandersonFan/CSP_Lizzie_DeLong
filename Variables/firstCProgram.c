8bc53a8e04f922df9f9c4becd10f9f21dad038c9
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-01-31 18:52:50 -0700

    Finshed First C program

--- Variables/firstCProgram.c
+++ Variables/firstCProgram.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
     char greating[] = "Hello! I would like to know you more! \n";
@@ -6,17 +7,22 @@ int main(void){
     printf("Would you please tell me your name:\n");
 
     char yourName[30];
-    scanf("%s", yourName);
+    fgets(yourName, sizeof(yourName), stdin);
+
+    // find out how long the name is
+    int nameLength = strlen(yourName);
+    // cut off the last character by making it be the NULL character (0)
+    yourName[nameLength-1] = 0;
  
 
     char favBook[60];
     printf("Great! Now tell me you favorite book: \n");
     fgets(favBook, sizeof(favBook), stdin);
-    //scanf("%s", favBook);
- 
-    
 
-    printf("Thank you! Now I know you name %s is and you like the book %s\n", yourName, favBook);
+    int bookLength = strlen(favBook);
+    favBook[bookLength-1] = 0;
+ 
+    printf("Thank you! Now I know your name is %s and you like the book %s!\n", yourName, favBook);
 
     return 0;
 }
