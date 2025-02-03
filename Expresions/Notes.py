3b652d9cc3d4bc1e23e9e702cf0cac85a455eac7
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-03 11:49:08 -0700

    MATH

---
+++ Expresions/Notes.py
@@ -0,0 +1,14 @@
+# Lizzie DeLong, Expresions Notes Python
+
+#print(70/8)
+#print(70+8)
+#print(70-8)
+#print(70*8)
+#print(70**8)
+#print(70//8) #Returns whole number
+#print(70%8) # % is called modulo/modulus/MOD - returns the remainder
+num = 4
+print(num)
+
+num //= 1
+print(num)
---
+++ Expresions/notes.c
@@ -0,0 +1,10 @@
+// Lizzie DeLong, Notes Expresions C
+
+#include <stdio.h>
+#include <math.h>
+float equation = 5*11+78/(5-2);
+
+int main(void){
+    //printf("%f \n", equation);
+    return 0;
+}
