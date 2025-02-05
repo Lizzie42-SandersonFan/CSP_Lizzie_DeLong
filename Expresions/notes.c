219efc86ba073438a0bb69e053ba6fd65464fa87
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-05 11:11:46 -0700

    More notes

--- Expresions/notes.c
+++ Expresions/notes.c
@@ -5,11 +5,13 @@
 
 //float x = pow(2,2);
 //float equation = 5*11+78/(5-x);
+float expressions = 5*pow(7, 2)/4;
 
 
 int main(void){
-    char name[] = "Lizzie";
-    printf("%s\n", name);
+    //char name[] = "Lizzie";
+    //printf("%s\n", name);
+    //printf("%f", expressions);
 
     return 0;
 }
---
+++ Expresions/practice.c
@@ -0,0 +1,9 @@
+// Lizzie DeLong, Expressions C practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
