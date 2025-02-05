f97fb573a9eece68c6ef13b77b91e58c62c250f7
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-05 11:14:37 -0700

    File Fixing

--- Expresions/notes.c
+++ Expresions/notes.c
@@ -11,7 +11,7 @@ float expressions = 5*pow(7, 2)/4;
 int main(void){
     //char name[] = "Lizzie";
     //printf("%s\n", name);
-    //printf("%f", expressions);
+    printf("%f", expressions);
 
     return 0;
 }
