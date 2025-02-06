a789c117bfdd71dc91ac66645af3f285277bf93d
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-06 11:15:18 -0700

    Mores stuff

---
+++ Expresions/financialCalculator.c
@@ -0,0 +1,9 @@
+// Lizzie DeLong, Financial Calculator C
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
---
+++ Expresions/financialCalculator.py
@@ -0,0 +1,5 @@
+# Lizzie DeLong, Financial Calculator Python
+
+# Things to include: monthly income, how much of income in savings, how much left to spend
+# Use income to calculate percent for each expence and savings
+# Input needed: Rent/Morgage, Utilities, Groceries
