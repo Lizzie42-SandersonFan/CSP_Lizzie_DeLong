6ebfca11edede2dfb42b207cd2df92ecc9050b05
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-06 20:24:55 +0000

    Started C (has bugs)

--- Expresions/financialCalculator.c
+++ Expresions/financialCalculator.c
@@ -5,42 +5,54 @@
 
 int main(void){
     // Print hi and tell user what this program does
-
+    printf("Hello! This is your new personal financial calculator to help you with your money!");
     // user input of income (variable)
-
+    float income[15];
+    printf("What is your monthly income: \n");
+    scanf("%f", income);
     // user input of rent (variable)
-
+    float rent[15];
+    printf("What is your monthly rent payment: \n");
+    scanf("%f", rent);
     // user input of utilities (variable)
-
+    float utilities[15];
+    printf("What is your monthly utilities payment: \n");
+    scanf("%f", utilities);
     // user input of groceries (variable)
-
+    float groceries[15];
+    printf("What is your monthly grocerie spending: \n");
+    scanf("%f", groceries);
     // user input of transportaion (variable)
-
+    float transport[15];
+    printf("What is your monthly transportation spending: \n");
+    scanf("%f", transport);
     // calculate savings as 10% of income(income * .1) (variable)
-
+    float savings[20] = income*0.1;
     // calculate spending as income-savings-rent-utilities-groceries-treansport
+    float spending = income-savings-rent-utilities-groceries-transport;
 
     // calculate percent income of rent (rent/income*100) (varible)
-
+    float rentPercent = (rent/income)*100;
     // calculate percent income of utilities (utilites/income*100) (varible)
-
+    float utilitiesPercent = (utilities/income)*100;
     // calculate percent income of groceries (groceries/income*100) (varible)
-
+    float groceriesPercent = (groceries/income)*100;
     // calculate percent income of transportaion (transportaion/income*100) (varible)
-
+    float transportPercent = (transport/income)*100;
     // calculate percent income of spending (spending/income*100) (varible)
+    float spendingPercent = (rent/income)*100;
 
     // your rent is $XX.XX which is XX% of your income. (print)
-
+    printf("Your rent bill is %f which is %f of your income. \n", rent, rentPercent);
     // your utilities is $XX.XX which is XX% of your income. (print)
-
+    printf("Your utilities bill is %f which is %f of your income. \n", utilities, utilitiesPercent);
     // your groceries is $XX.XX which is XX% of your income. (print)
-
+    printf("Your groceries spending is %f which is %f of your income. \n", groceries, groceriesPercent);
     // your transportion is $XX.XX which is XX% of your income. (print)
-
+    printf("Your tranportstion cost is %f which is %f of your income. \n", transport, transportPercent);
     // your savings is $XX.XX which is XX% of your income. (print)
-
+    printf("The monthly savings you should put away is %f. \n", savings);
     // your spending is $XX.XX which is XX% of your income. (print)
-
+    printf("Your spending money left is %f which is %f of your income", spending, spendingPercent);
     return 0;
 }
--- Expresions/notes.c
+++ Expresions/notes.c
@@ -5,11 +5,11 @@
 
 //float x = pow(2,2);
 //float equation = 5*11+78/(5-x);
-float expressions = 5*pow(7, 2)/4;
+
 
 
 int main(void){
-    
+    float expressions = 5*pow(7, 2)/4;
     //char name[] = "Lizzie";
     //printf("%s\n", name);
     printf("%f", expressions);
