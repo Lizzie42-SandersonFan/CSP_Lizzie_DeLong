27e7013a0fb3ab1be930f5dcb97a8d376699d9ae
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-06 11:48:27 -0700

    Started python

--- Expresions/financialCalculator.c
+++ Expresions/financialCalculator.c
@@ -4,6 +4,43 @@
 #include <math.h>
 
 int main(void){
-    printf("Hello World");
+    // Print hi and tell user what this program does
+
+    // user input of income (variable)
+
+    // user input of rent (variable)
+
+    // user input of utilities (variable)
+
+    // user input of groceries (variable)
+
+    // user input of transportaion (variable)
+
+    // calculate savings as 10% of income(income * .1) (variable)
+
+    // calculate spending as income-savings-rent-utilities-groceries-treansport
+
+    // calculate percent income of rent (rent/income*100) (varible)
+
+    // calculate percent income of utilities (utilites/income*100) (varible)
+
+    // calculate percent income of groceries (groceries/income*100) (varible)
+
+    // calculate percent income of transportaion (transportaion/income*100) (varible)
+
+    // calculate percent income of spending (spending/income*100) (varible)
+
+    // your rent is $XX.XX which is XX% of your income. (print)
+
+    // your utilities is $XX.XX which is XX% of your income. (print)
+
+    // your groceries is $XX.XX which is XX% of your income. (print)
+
+    // your transportion is $XX.XX which is XX% of your income. (print)
+
+    // your savings is $XX.XX which is XX% of your income. (print)
+
+    // your spending is $XX.XX which is XX% of your income. (print)
+
     return 0;
 }
