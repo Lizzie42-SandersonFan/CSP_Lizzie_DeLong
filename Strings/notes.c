7a99d96dfaf0617580dae65fd51d746f47e845a7
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-11 11:48:29 -0700

    Started Final project

--- Strings/notes.c
+++ Strings/notes.c
@@ -3,8 +3,36 @@
 #include <stdio.h>
 #include <string.h>
 
+char subject[50];
+char name[] = "Victoria";
+char sentence[] = "The quick brown fox jumps over the lazy dog.";
+
 int main(void){
+    //printf("What class are you in: \n");
+    //fgets(subject, sizeof(subject), stdin);
+    //int subjectLength = strlen(subject);
+    //subject[subjectLength-1] = 0;
+    //printf("You are in %s, that is a cool class.", subject);
+    //name[0] = 'T';
+    //name[1] = 'o';
+    //name[2] = 'r';
+    //name[3] = 'i';
+    //name[4] = ' ';
+    //name[5] = ' ';
+    //name[6] = ' ';
+    //name[7] = ' ';
+    //printf("%lu\n",sizeof(sentence));
+    //printf("%d\n",strlen(sentence));
+
+    char one[] = "Hello ";
+    char two[] = "World!";
+    char three[] = "Welcome to my program. ";
     
-    printf("Hello World");
+    printf("%s\n", one);
+    strcat(one, two);
+    printf("%s\n", one);
+    strcat(three, one);
+    printf("%s\n", three);
+
     return 0;
 }
--- Strings/sillySentance.py
+++ Strings/sillySentance.py
@@ -1,2 +1,8 @@
 # Lizzie DeLong, Silly Sentances Python
 
+# Need three inputs to use and put into a MidLibs thing. Try and find a way to use cancatination
+#madlib outline
+
+madLibSectionOne = "idk yet"
+madLibSectionTwo = "idk yet"
+madLibSectionThree = "idk yet"
