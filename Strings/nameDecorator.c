5729475f4f4e5d706afcdb3712a56535239f85a8
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-11 02:01:17 +0000

    Finished C practice

--- Strings/nameDecorator.c
+++ Strings/nameDecorator.c
@@ -6,19 +6,26 @@
 int main(void){
     printf("Hello! This is a program you can use to give your name some pazazz. \n");
 
-    char name[30];
+    char name[50];
     printf("Would you please tell me your name: \n");
     fgets(name, sizeof(name), stdin);
+    int nameLength = strlen(name);
+    name[nameLength-1] = 0;
 
     char decor[5];
     printf("And what symbol would you like around your name(one symbol and no numbers please): \n");
     scanf("%s", decor);
 
-    strcat(decor, decor);
-    strcat(decor, name);
-    strcat(decor, name);
-    //strcat(decor, decor);
+    char finalString[70] = {0};
 
-    printf("%s", decor);
+    strcat(finalString, decor);
+    strcat(finalString, decor);
+    strcat(finalString, decor);
+    strcat(finalString, name);
+    strcat(finalString, decor);
+    strcat(finalString, decor);
+    strcat(finalString, decor);
+
+    printf("%s", finalString);
     return 0;
 }
