0bf9feddf815398c33300354de93cfb3e9844bfc
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-12 11:48:50 -0700

    AHHHH

--- Strings/sillySentance.py
+++ Strings/sillySentance.py
@@ -1,8 +1,16 @@
 # Lizzie DeLong, Silly Sentances Python
 
 # Need three inputs to use and put into a MidLibs thing. Try and find a way to use cancatination
-#madlib outline
+# NAME was off to school, and to get there, they rode a(n) ANIMAL to get there. 
+# After completing the first half of their classes, they got to eat FOOD for lunch.
 
-madLibSectionOne = "idk yet"
-madLibSectionTwo = "idk yet"
-madLibSectionThree = "idk yet"
+print("Hello! This is a program that is a mini MadLibs.")
+print("This woun't take very long, just a few minutes!")
+aName = input("Need a Name:\n").strip().capitalize()
+madLibSectionOne = " was off to school, and to get there, they rode a(n) "
+anAnimal = input("Now an Animal:\n").strip().capitalize()
+madLibSectionTwo = " to get there. "
+madLibSectionThree = "After completing the first half of their classes, they got to eat "
+aFood = input("Lastly, a Food:\n").strip().capitalize()
+madLibSectionFour = " for lunch."
+print(aName+madLibSectionOne+anAnimal+madLibSectionTwo+madLibSectionThree+aFood+madLibSectionFour)
--- Strings/sillySentances.c
+++ Strings/sillySentances.c
@@ -4,6 +4,34 @@
 #include <string.h>
 
 int main(void){
-    printf("Hello World");
+    printf("Hello! This is a program that is a mini MadLibs.\n");
+    printf("This woun't take very long, just a few minutes!\n");
+    char aName[20];
+    printf("Need a Name:\n");
+    scanf("%s",aName);
+    
+    char anAnimal[20];
+    printf("Now and Animal:\n");
+    scanf("%s",anAnimal);
+
+    char aFood[20];
+    printf("Finaly, a Food:\n");
+    scanf("%s",aFood);
+
+    char madLibSectionOne[] = " was off to school, and to get there, they rode a(n) ";
+    char madLibSectionTwo[] = " to get there. \n";
+    char madLibSectionThree[] = "After completing the first half of their classes, they got to eat ";
+    char madLibSectionFour[] = " for lunch.";
+
+    char finalLongString[150] = {0};
+    strcat(finalLongString, aName);
+    strcat(finalLongString, madLibSectionOne);
+    strcat(finalLongString, anAnimal);
+    strcat(finalLongString, madLibSectionTwo);
+    strcat(finalLongString, madLibSectionThree);
+    strcat(finalLongString, aFood);
+    strcat(finalLongString, madLibSectionFour);
+
+    printf("%s", finalLongString);
     return 0;
 }
