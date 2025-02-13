3848e135960f5317569fc424c8b4015eac307fde
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-13 11:37:22 -0700

    Finished C functions

--- Functions/updatedHelloWorld.c
+++ Functions/updatedHelloWorld.c
@@ -1,1 +1,26 @@
 // Lizzie DeLong, Updated Hello World C with Functions
+
+#include <stdio.h>
+
+void greet(char name[]) {
+    printf("Hello, %s!\n", name);
+}
+
+int main(void){
+    char nameOne[] = "Lizzie";
+    greet(nameOne);
+
+    char nameTwo[] = "Alice";
+    greet(nameTwo);
+
+    char nameThree[] = "John";
+    greet(nameThree);
+
+    char nameFour[] = "Bob";
+    greet(nameFour);
+
+    char nameFive[] = "Lila";
+    greet(nameFive);
+
+    return 0;
+}
