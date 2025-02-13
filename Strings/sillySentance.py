4969a9ee1aeef2004aba4a839ed82f0f1ae7eb6b
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-12 17:17:43 -0700

    Fixed Stuff

--- Strings/sillySentance.py
+++ Strings/sillySentance.py
@@ -7,10 +7,12 @@
 print("Hello! This is a program that is a mini MadLibs.")
 print("This woun't take very long, just a few minutes!")
 aName = input("Need a Name:\n").strip().capitalize()
-madLibSectionOne = " was off to school, and to get there, they rode a(n) "
 anAnimal = input("Now an Animal:\n").strip().capitalize()
-madLibSectionTwo = " to get there. "
-madLibSectionThree = "After completing the first half of their classes, they got to eat "
 aFood = input("Lastly, a Food:\n").strip().capitalize()
+
+madLibSectionOne = " was off to school, and to get there, they rode a(n) "
+madLibSectionTwo = " to get there. \n"
+madLibSectionThree = "After completing the first half of their classes, they got to eat "
 madLibSectionFour = " for lunch."
+
 print(aName+madLibSectionOne+anAnimal+madLibSectionTwo+madLibSectionThree+aFood+madLibSectionFour)
