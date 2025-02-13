0c044be2c371e71f39f446aab8dbd248ddabd991
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-13 11:27:50 -0700

    Finished Python function

--- Functions/updatedHelloWorld.py
+++ Functions/updatedHelloWorld.py
@@ -1,1 +1,12 @@
 # Lizzie DeLong, Updated Hello World Python with Functions
+
+name = input("What is your name:\n")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet(name)
+greet("Alice")
+greet("John")
+greet("Bob")
+greet("Lila")
