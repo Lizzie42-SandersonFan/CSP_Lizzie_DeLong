1183a78723db3e4d42906a8b031f4a5f47b9b4ed
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-19 20:08:11 +0000

    Finished the updated calculators

--- Functions/updatedCalculator.c
+++ Functions/updatedCalculator.c
@@ -3,56 +3,41 @@
 #include <stdio.h>
 #include <math.h>
 
+float userInputs(float expense[]){
+    printf("What is your monthly %s:\n", expense);
+}
+
+float calculatePercent(float expenseOne, float expenseTwo){
+    return (expenseOne/expenseTwo)*100;
+}
+
 int main(void){
-    // Print hi and tell user what this program does
     printf("Hello! This is your new personal financial calculator to help you with your money! \n");
-    // user input of income (variable)
-    float income;
-    printf("What is your monthly income: \n");
+    
+    float income = userInputs("income");
     scanf("%f", &income);
-    // user input of rent (variable)
-    float rent;
-    printf("What is your monthly rent payment: \n");
+    float rent = userInputs("rent/morage payment");
     scanf("%f", &rent);
-    // user input of utilities (variable)
-    float utilities;
-    printf("What is your monthly utilities payment: \n");
+    float utilities = userInputs("utilities payment");
     scanf("%f", &utilities);
-    // user input of groceries (variable)
-    float groceries;
-    printf("What is your monthly grocerie spending: \n");
+    float groceries = userInputs("groceries spending");
     scanf("%f", &groceries);
-    // user input of transportaion (variable)
-    float transport;
-    printf("What is your monthly transportation spending: \n");
+    float transport = userInputs("transportation spending");
     scanf("%f", &transport);
-    // calculate savings as 10% of income(income * .1) (variable)
     float savings = income*0.1;
-    // calculate spending as income-savings-rent-utilities-groceries-treansport
     float spending = income-savings-rent-utilities-groceries-transport;
 
-    // calculate percent income of rent (rent/income*100) (varible)
-    float rentPercent = (rent/income)*100;
-    // calculate percent income of utilities (utilites/income*100) (varible)
-    float utilitiesPercent = (utilities/income)*100;
-    // calculate percent income of groceries (groceries/income*100) (varible)
-    float groceriesPercent = (groceries/income)*100;
-    // calculate percent income of transportaion (transportaion/income*100) (varible)
-    float transportPercent = (transport/income)*100;
-    // calculate percent income of spending (spending/income*100) (varible)
-    float spendingPercent = (rent/income)*100;
+    float rentPercent = calculatePercent(rent, income);
+    float utilitiesPercent = calculatePercent(utilities, income);
+    float groceriesPercent = calculatePercent(groceries, income);
+    float transportPercent = calculatePercent(transport, income);
+    float spendingPercent = calculatePercent(spending, income);
 
-    // your rent is $XX.XX which is XX% of your income. (print)
-    printf("Your rent bill is $%f which is %f of your income. \n", rent, rentPercent);
-    // your utilities is $XX.XX which is XX% of your income. (print)
-    printf("Your utilities bill is $%f which is %f of your income. \n", utilities, utilitiesPercent);
-    // your groceries is $XX.XX which is XX% of your income. (print)
-    printf("Your groceries spending is $%f which is %f of your income. \n", groceries, groceriesPercent);
-    // your transportion is $XX.XX which is XX% of your income. (print)
-    printf("Your tranportstion cost is $%f which is %f of your income. \n", transport, transportPercent);
-    // your savings is $XX.XX which is XX% of your income. (print)
-    printf("The monthly savings you should put away is $%f. \n", savings);
-    // your spending is $XX.XX which is XX% of your income. (print)
-    printf("Your spending money left is %%f which is %f of your income", spending, spendingPercent);
+    printf("Your rent bill is $%f which is %f percent of your income. \n", rent, rentPercent);
+    printf("Your utilities bill is $%f which is %f percent of your income. \n", utilities, utilitiesPercent);
+    printf("Your groceries spending is $%f which is %f percent of your income. \n", groceries, groceriesPercent);
+    printf("Your tranportstion cost is $%f which is %f percent of your income. \n", transport, transportPercent);
+    printf("The monthly savings you should put away is $%f, which is 10 percent of your income. \n", savings);
+    printf("Your spending money left is %%f which is %f percent of your income.", spending, spendingPercent);
     return 0;
 }
--- Functions/updatedCalculator.py
+++ Functions/updatedCalculator.py
@@ -5,42 +5,28 @@ def userInputs(expense):
 
 def calculatePercents(expenceOne, expenseTwo):
     return float(expenceOne/expenseTwo)*100
-# Print hi and tell user what this program does
+
 print("Hello! This is your new personal financial calculator to help you with your money!")
-# user input of income (variable)
+
 income = userInputs("income")
-# user input of rent (variable)
 rent = userInputs("rent/morage payment")
-# user input of utilities (variable)
 utilities = userInputs("utilities payment")
-# user input of groceries (variable)
 groceries = userInputs("groceries spending")
-# user input of transportaion (variable)
 transport = userInputs("transportaion spending")
-# calculate savings as 10% of income(income * .1) (variable)
 savings = income*0.1
-# calculate spending as income-savings-rent-utilities-groceries-treansport
 spending = income-savings-rent-utilities-groceries-transport
-# calculate percent income of rent (rent/income*100) (varible)
+
 rentPercent = calculatePercents(rent, income)
-# calculate percent income of utilities (utilites/income*100) (varible)
 utilitiesPercent = calculatePercents(utilities, income)
-# calculate percent income of groceries (groceries/income*100) (varible)
 groceriesPercent = calculatePercents(groceries, income)
-# calculate percent income of transportaion (transportaion/income*100) (varible)
 transportPercent = calculatePercents(transport, income)
-# calculate percent income of spending (spending/income*100) (varible)
 spendingPercent = calculatePercents(spending, income)
+
 answer = input("Horray! Ready for your results? \n")
-# your rent is $XX.XX which is XX% of your income. (print)
+
 print("Your rent is", rent, "dollars which is", rentPercent, "percent of your income.")
-# your utilities is $XX.XX which is XX% of your income. (print)
 print("Your utilities bill is", utilities, "dollars which is", utilitiesPercent, "percent of your income.")
-# your groceries is $XX.XX which is XX% of your income. (print)
 print("Your groceries spending is", groceries, "dollars which is", groceriesPercent, "percent of your income.")
-# your transportion is $XX.XX which is XX% of your income. (print)
 print("Your transportion cost is", transport, "dollars which is", transportPercent, "percent of your income.")
-# your savings is $XX.XX which is XX% of your income. (print)
 print("The savings you should put away every month is", savings, "which is 10 percent of your income.")
-# your spending is $XX.XX which is XX% of your income. (print)
 print("Your spending money is", spending, "dollars which is", spendingPercent, "percent of your income.")
