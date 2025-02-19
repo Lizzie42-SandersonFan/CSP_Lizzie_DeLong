198577b76201ecb339014beff0e1a850c1726aca
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-19 11:49:06 -0700

    Finish Python cal with funcs

--- Functions/updatedCalculator.py
+++ Functions/updatedCalculator.py
@@ -1,51 +1,46 @@
 # Lizzie DeLong, Updated Python Financial Claculator with Functions
 
-def userInputs():
-    expenses = []
-    expenseTypes = ["rent", "utilities", "groceries", "transportation"]
+def userInputs(expense):
+    return float(input(f"What is your monthly {expense}:\n"))
 
-    for expense in expenseTypes:
-        amount = float(input(f"What is your monthly {expense}:\n"))
-        expenses.append(amount)
-
-    return expenses
-
-def percentOfExpenses(expenses, income):
-    totalExpenses = sum(expenses)
-    percentages = [(expense/income)*100 for expense in expenses]
-
-    return totalExpenses, percentages
-
-
-
+def calculatePercents(expenceOne, expenseTwo):
+    return float(expenceOne/expenseTwo)*100
 # Print hi and tell user what this program does
 print("Hello! This is your new personal financial calculator to help you with your money!")
 # user input of income (variable)
-income = float(input("What is your monthly income? \n"))
+income = userInputs("income")
+# user input of rent (variable)
+rent = userInputs("rent/morage payment")
+# user input of utilities (variable)
+utilities = userInputs("utilities payment")
+# user input of groceries (variable)
+groceries = userInputs("groceries spending")
+# user input of transportaion (variable)
+transport = userInputs("transportaion spending")
 # calculate savings as 10% of income(income * .1) (variable)
 savings = income*0.1
 # calculate spending as income-savings-rent-utilities-groceries-treansport
 spending = income-savings-rent-utilities-groceries-transport
 # calculate percent income of rent (rent/income*100) (varible)
-rentPercent = (rent/income)*100
+rentPercent = calculatePercents(rent, income)
 # calculate percent income of utilities (utilites/income*100) (varible)
-utilitiesPercent = (utilities/income)*100
+utilitiesPercent = calculatePercents(utilities, income)
 # calculate percent income of groceries (groceries/income*100) (varible)
-groceriesPercent = (groceries/income)*100
+groceriesPercent = calculatePercents(groceries, income)
 # calculate percent income of transportaion (transportaion/income*100) (varible)
-transportPercent = (transport/income)*100
+transportPercent = calculatePercents(transport, income)
 # calculate percent income of spending (spending/income*100) (varible)
-spendingPercent = (spending/income)*100
+spendingPercent = calculatePercents(spending, income)
 answer = input("Horray! Ready for your results? \n")
 # your rent is $XX.XX which is XX% of your income. (print)
-print("Your rent is", rent, "which is", rentPercent, "of your income.")
+print("Your rent is", rent, "dollars which is", rentPercent, "percent of your income.")
 # your utilities is $XX.XX which is XX% of your income. (print)
-print("Your utilities bill is", utilities, "which is", utilitiesPercent, "of your income.")
+print("Your utilities bill is", utilities, "dollars which is", utilitiesPercent, "percent of your income.")
 # your groceries is $XX.XX which is XX% of your income. (print)
-print("Your groceries spending is", groceries, "which is", groceriesPercent, "of your income.")
+print("Your groceries spending is", groceries, "dollars which is", groceriesPercent, "percent of your income.")
 # your transportion is $XX.XX which is XX% of your income. (print)
-print("Your transportion cost is", transport, "which is", transportPercent, "of your income.")
+print("Your transportion cost is", transport, "dollars which is", transportPercent, "percent of your income.")
 # your savings is $XX.XX which is XX% of your income. (print)
 print("The savings you should put away every month is", savings, "which is 10 percent of your income.")
 # your spending is $XX.XX which is XX% of your income. (print)
-print("Your spending money is", spending, "which is", spendingPercent, "of your income.")
+print("Your spending money is", spending, "dollars which is", spendingPercent, "percent of your income.")
