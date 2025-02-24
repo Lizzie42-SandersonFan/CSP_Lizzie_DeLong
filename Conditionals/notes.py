1786733e2e8d6637a4e9eed66671430ce11ae67c
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-24 11:48:35 -0700

    Notes

--- Conditionals/notes.py
+++ Conditionals/notes.py
@@ -1,2 +1,29 @@
 # Lizzie DeLong, Python Conditional Notes
+name = input("What is your name:\n")
 
+if name == "LaRose":
+    print("Hi Ms. LaRose")
+else:
+    print(f"Hello {name}!")
+
+num = int(input("Give me a number:\n"))
+
+if num >= -1000000000000000000000000000000000000000000000000000000000000000000000000000000000:
+    print("That is a negitive number!")
+elif num == 0:
+    print("There are none.")
+elif num == 1:
+    print("There is one.")
+elif num <= 4:
+    print("There are a couple.")
+elif num <= 10:
+    print("There are a few.")
+else:
+    print("There are many!")
+
+if num < 10 and num > -1:
+    print("This is a big single digit number")
+
+if num < 10:
+    if num == 8:
+        print("This prints at 8")
