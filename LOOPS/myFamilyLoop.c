0811c82b522f1b17460575660635c9c1a29e5d1c
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-27 11:49:13 -0700

    LOOPS WORK

--- LOOPS/myFamilyLoop.c
+++ LOOPS/myFamilyLoop.c
@@ -2,7 +2,11 @@
 
 #include <stdio.h>
 
+char famList [4][10] = {"Lizzie", "Daniel", "Sarah", "Dave"};
+int i = 0;
 int main(void){
-    printf("Hello World");
+    for(i = 0; i < 4; i++){
+        printf("Hello %s!\n", famList);
+    }
     return 0;
 }
--- LOOPS/myFamilyLoop.py
+++ LOOPS/myFamilyLoop.py
@@ -1,2 +1,6 @@
 # Lizzie DeLong, Family Loop Python
 
+famList = ["Lizzie", "Daniel", "Sarah(mom)", "Dave(dad)"]
+
+for item in famList:
+    print(f"Hello {item}!")
