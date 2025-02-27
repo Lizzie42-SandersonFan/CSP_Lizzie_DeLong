ea43adc86d8713fb5e24061af2311b168ddceea3
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-27 20:34:33 +0000

    Finished famLoop C

--- LOOPS/myFamilyLoop.c
+++ LOOPS/myFamilyLoop.c
@@ -6,7 +6,7 @@ int main(void){
     char famList [4][10] = {"Daniel", "Lizzie", "Sarah", "Dave"};
     int i;
     for(i = 0; i < 4; i++){
-        printf("Hello %s!\n", famList);
+        printf("Hello %s!\n", famList[i]);
     }
     return 0;
 }
