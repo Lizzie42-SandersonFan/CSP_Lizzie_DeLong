62633105490d3440147647ba14299addfb82a06f
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-28 20:35:58 +0000

    For fun code

---
+++ MyPersonalReferanceCode/ChalengeCodeOne.c
@@ -0,0 +1,13 @@
+// Another challenge like the "IWasBored" file
+#include <stdio.h>
+
+int main(void){
+    char instruments [][30] = {"Violin", "Viola", "Cello", "Bass", "Tuba", "Trombone", "Drums", "Guitar", "Flute", "Trumpet", "Clairanet", "Piano", "Harp"};
+    size_t myArray = sizeof(instruments);
+    int i;
+    printf(myArray);
+    //for(i = 0; i <= instruments; i++){
+        //printf("An instrument is: %s.\n", instruments[i]);
+    //}
+    return 0;
+} 
---
+++ MyPersonalReferanceCode/IWasBored.c
@@ -0,0 +1,12 @@
+// This is some code I did 'cause I was Bored.
+// Trying to do a loop that goes from end to begining
+#include <stdio.h>
+
+int main(void){
+    char candies [13][40] = {"Kisses", "Chocolate Bar", "Skittles", "M&M's", "Orange Sticks", "Milky Ways", "Snickers", "Mars Bars", "Lemon Drop", "Nerds", "Sweet Tarts", "Twizzlers", "Dum-Dums"};
+    int i;
+    for(i = 12; i > -1; i--){
+        printf("A candy is: %s.\n", candies[i]);
+    }
+    return 0;
+}
