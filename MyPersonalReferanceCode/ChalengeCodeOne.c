50e5ba45a8bdaf4842d0af0540d709d11a47dd26
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-02-28 20:41:22 +0000

    Tried stuff

--- MyPersonalReferanceCode/ChalengeCodeOne.c
+++ MyPersonalReferanceCode/ChalengeCodeOne.c
@@ -3,9 +3,10 @@
 
 int main(void){
     char instruments [][30] = {"Violin", "Viola", "Cello", "Bass", "Tuba", "Trombone", "Drums", "Guitar", "Flute", "Trumpet", "Clairanet", "Piano", "Harp"};
-    size_t myArray = sizeof(instruments);
-    int i;
-    printf(myArray);
+    size_t my_array = sizeof(instruments);
+    int list_length = sizeof(my_array) / sizeof(my_array[0]);
+    //int i;
+    printf(list_length);
     //for(i = 0; i <= instruments; i++){
         //printf("An instrument is: %s.\n", instruments[i]);
     //}
