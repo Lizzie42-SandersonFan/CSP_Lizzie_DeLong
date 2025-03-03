0ba67dac374652d80a065a13831341799e1eda4b
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-03-03 11:43:16 -0700

    Notes and the one Dad Challenge

--- LOOPS/notes.py
+++ LOOPS/notes.py
@@ -1,2 +1,16 @@
 # Lizzie DeLong, Loops Notes Python
 
+nums = [2,4,6,8,10]
+
+for num in nums:
+    print(num)
+
+print("End of for loop")
+
+x = 0
+
+while x < 10:
+    print(x)
+    x += 1
+
+print("End of while loop")
--- MyPersonalReferanceCode/ChalengeCodeOne.c
+++ MyPersonalReferanceCode/ChalengeCodeOne.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 int main(void){
-    char instruments [][30] = {"Violin", "Viola", "Cello", "Bass", "Tuba", "Trombone", "Drums", "Guitar", "Flute", "Trumpet", "Clairanet", "Piano", "Harp"};
+    char instruments [][30] = {"Violin", "Viola", "Cello", "Bass", "Tuba", "Trombone", "Drums", "Guitar", "Flute", "Trumpet", "Clairanet", "Piano", "Harp", "Oboe"};
+
     size_t my_array = sizeof(instruments);
-    int list_length = sizeof(my_array) / sizeof(my_array[0]);
-    //int i;
-    printf(list_length);
-    //for(i = 0; i <= instruments; i++){
-        //printf("An instrument is: %s.\n", instruments[i]);
-    //}
+    int list_length = my_array / 30;
+    
+    int i;
+    for(i = 0; i < list_length; i++){
+        printf("An instrument is: %s.\n", instruments[i]);
+    }
     return 0;
 } 
