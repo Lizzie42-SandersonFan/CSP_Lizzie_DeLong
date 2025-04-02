1ce6278379b04719f98ee1eb2ba018ddb19a8aad
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-04-02 19:41:02 +0000

    Started working on cafe game

---
+++ MyPersonalReferanceCode/cafe.py
@@ -0,0 +1,13 @@
+# Like the make cakes game but you set up a cafe!
+
+# have user pick a name for cafe
+# is a cat cafe?
+# what kind of drinks and pasteries will be sold?
+# drive thru?
+# dogs and other pets allowed? what about service animals?
+# hours open?
+# objective: be better than the coffee shop accross the street!
+
+print("Hello there!")
+userName = input("Would you please tell me your name?").capitalize()
+
