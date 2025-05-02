3060b151ac6043798e33b39b80012e4b22b31307
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-05-01 18:03:37 -0600

    Fully Finished!

--- Internet_Final/final.js
+++ Internet_Final/final.js
@@ -1,9 +1,14 @@
-let imageThing=document.getElementById("explain_img");
-let goHereImages = ["https://www.colemanconcierge.com/wp-content/uploads/2023/11/Park-City-Utah-in-winter-via-Canva.jpg", "https://www.parkcityinformation.org/images/main-street-park-city.jpg", "https://gallery.streamlinevrs.com/units-gallery/00/07/D1/thumbnail_160744164.jpeg"]
-x = 0;
+
+let goHereImages = ["https://www.colemanconcierge.com/wp-content/uploads/2023/11/Park-City-Utah-in-winter-via-Canva.jpg", "https://www.parkcityinformation.org/images/main-street-park-city.jpg", "https://gallery.streamlinevrs.com/units-gallery/00/07/D1/thumbnail_160744164.jpeg"];
+let x = 0;
 
 function change() {
-    x<goHereImages.length?x+=1:x=0; 
+    if (x < goHereImages.length-1) {
+        x += 1;
+    } else {
+        x = 0;
+    }
+    let imageThing=document.getElementById("explain_img");
     imageThing.src=goHereImages[x];
 }
 
@@ -19,18 +24,19 @@ setInterval(change, 3000);
     //}
 //}
 
-const imageTest = document.getElementById("food_img");
 let foodImages = ["https://www.femalefoodie.com/wp-content/uploads/2019/06/five-5eeds-9.jpg", "https://images.squarespace-cdn.com/content/v1/6274079d047d971acb504c55/7c9fd062-63c2-493d-8ed5-4f902d8b9d6e/1.29.2022---Tupelo-173.jpg", "https://scene7.vailresorts.com/is/image/vailresorts/20230106_PC_Tingey_001:Categories?fmt=png-alpha&resMode=sharp2&w=429&h=369&wid=382&fit=constrain,1&dpr=on,2.625"]
-let currentPos = 0;
+let i = 0;
 
-function volgendefoto() {
-    if (++currentPos >= foodImages.length)
-        currentPos = 0;
-    
-    imageTest.src = foodImages[currentPos];
+function changeAgain() {
+    if (i < foodImages.length-1) {
+        i += 1;
+    } else {
+        i = 0;
+    }
+    let images=document.getElementById("food_img");
+    images.src=foodImages[i];
 }
-
-setInterval(volgendefoto, 3000);
+setInterval(changeAgain, 3000);
 //let i = 0
 //function changeImgThree(){
     //if (i < foodImages.length){
@@ -46,13 +52,14 @@ let coolThingsImg = ["https://abodeparkcity.com/wp-content/uploads/2022/06/snowp
 let counter = 0
 function changeImgTwo(){
     if (counter < coolThingsImg.length){
-        document.getElementById("neat_img").src = coolThingsImg[counter]
         counter += 1
     } else {
         counter = 0
-        document.getElementById("neat_img").src = coolThingsImg[counter]
     }
+    let image = document.getElementById("neat_img")
+    image.src=coolThingsImg[i];
 }
+setInterval(changeImgTwo, 3000)
 
 function viewStuff(){
     if(document.getElementById("hidden_stuff").style.display === "block"){
